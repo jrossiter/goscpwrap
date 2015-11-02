f33afcbdc6344e00154a3baf485a2913f048674d
Jason Rossiter <jrossiter403@gmail.com>
2015-11-02 22:01:11 +1100

    Added tests for handleItem.

--- src/goscp/goscp_test.go
+++ src/goscp/goscp_test.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -40,7 +41,6 @@ func setUp() {
 
 func tearDown() {
 	for _, v := range created {
-		log.Println("Removing", v)
 		os.Remove(v)
 	}
 }
@@ -55,14 +55,17 @@ func TestUpDirectory(t *testing.T) {
 		Expected []string
 	}{
 		{
+			// Up one directory
 			Input:    []string{"one", "two", "three"},
 			Expected: []string{"one", "two"},
 		},
 		{
+			// Up one directory
 			Input:    []string{"one"},
 			Expected: []string{},
 		},
 		{
+			// Current directory
 			Input:    []string{},
 			Expected: []string{},
 		},
@@ -88,6 +91,7 @@ func TestParseMessage(t *testing.T) {
 		ExpectedError string
 	}{
 		{
+			// Create file message
 			Input: "C0644 25 helloworld.txt",
 			Regex: fileCopyRx,
 			Expected: map[string]string{
@@ -98,6 +102,7 @@ func TestParseMessage(t *testing.T) {
 			},
 		},
 		{
+			// Create directory message
 			Input: "D0755 0 mydir",
 			Regex: dirCopyRx,
 			Expected: map[string]string{
@@ -108,6 +113,7 @@ func TestParseMessage(t *testing.T) {
 			},
 		},
 		{
+			// Timestamp message
 			Input: "T1234567890 0 9876543210 0",
 			Regex: timestampRx,
 			Expected: map[string]string{
@@ -117,6 +123,7 @@ func TestParseMessage(t *testing.T) {
 			},
 		},
 		{
+			// Invalid message
 			Input:         "Invalid msg",
 			Regex:         fileCopyRx,
 			ExpectedError: "Could not parse protocol message: Invalid msg",
@@ -142,7 +149,7 @@ func TestParseMessage(t *testing.T) {
 
 func TestDirectory(t *testing.T) {
 	uts := time.Now().Unix()
-	dirName := fmt.Sprintf("%s-%v", "mydir", uts)
+	dirName := fmt.Sprintf("%s-%v", "goscp-mydir", uts)
 
 	tests := []struct {
 		StartPath               string
@@ -151,6 +158,7 @@ func TestDirectory(t *testing.T) {
 		ExpectedDestinationPath []string
 	}{
 		{
+			// Directory message
 			StartPath:               ".",
 			InputPath:               fmt.Sprintf("D0755 0 %s", dirName),
 			ExpectedPath:            dirName,
@@ -190,12 +198,14 @@ func TestFile(t *testing.T) {
 		ExpectedPath string
 	}{
 		{
+			// File message
 			StartPath:    ".",
 			InputPath:    fmt.Sprintf("C0755 %d %s", len(fileContent), fileName),
 			FileContent:  fileContent,
 			ExpectedPath: fileName,
 		},
 		{
+			// Empty file message
 			StartPath:    ".",
 			InputPath:    fmt.Sprintf("C0755 %d %s", 0, fileName),
 			FileContent:  "",
@@ -229,10 +239,150 @@ func TestFile(t *testing.T) {
 	}
 }
 
+func TestHandleItem(t *testing.T) {
+	tests := []struct {
+		Type                    string
+		Name                    string
+		Content                 []byte
+		ExpectedMessages        []string
+		DestinationPath         []string
+		ExpectedDestinationPath []string
+	}{
+		{
+			// File creation
+			Type:    "file",
+			Name:    "goscp-test-content.txt",
+			Content: []byte("hello-world test text\n"),
+			ExpectedMessages: []string{
+				"C0644 22 goscp-test-content.txt\n",
+				"hello-world test text\n",
+				"\x00\n",
+			},
+		},
+		{
+			// Empty file creation
+			Type:    "file",
+			Name:    "goscp-test-content.txt",
+			Content: []byte(""),
+			ExpectedMessages: []string{
+				"C0644 0 goscp-test-content.txt\n",
+				"\x00\n",
+			},
+		},
+
+		{
+			// Directory creation and traversing upward
+			Type: "directory",
+			Name: "goscp-test-dir/two",
+			ExpectedMessages: []string{
+				"E\n",
+				"E\n",
+				"D0644 0 two\n",
+			},
+			DestinationPath:         []string{"goscp-test-dir", "hello", "one"},
+			ExpectedDestinationPath: []string{"goscp-test-dir/two"},
+		},
+		{
+			// Directory creation in same level
+			Type: "directory",
+			Name: "goscp-test-dir/one",
+			ExpectedMessages: []string{
+				"E\n",
+				"D0644 0 one\n",
+			},
+			DestinationPath:         []string{"goscp-test-dir", "two"},
+			ExpectedDestinationPath: []string{"goscp-test-dir/one"},
+		},
+		{
+			// Directory creation in traversing downward
+			Type: "directory",
+			Name: "goscp-test-dir/one/two",
+			ExpectedMessages: []string{
+				"D0644 0 two\n",
+			},
+			DestinationPath:         []string{"goscp-test-dir", "one"},
+			ExpectedDestinationPath: []string{"goscp-test-dir/one/two"},
+		},
+		{
+			// Directory creation
+			Type: "directory",
+			Name: "goscp-test-dir",
+			ExpectedMessages: []string{
+				"E\n",
+				"D0644 0 goscp-test-dir\n",
+			},
+			DestinationPath:         []string{"."},
+			ExpectedDestinationPath: []string{"goscp-test-dir"},
+		},
+	}
+
+	for _, v := range tests {
+		r, w := io.Pipe()
+		c := Client{
+			scpStdinPipe:    w,
+			ShowProgressBar: false,
+		}
+
+		filePath := v.Name
+		var stats os.FileInfo
+		if v.Type == "file" {
+			f, err := os.Create(filePath)
+			if err != nil {
+				t.Error("Unexpected error:", err)
+			}
+
+			f.Write(v.Content)
+			f.Close()
+		} else if v.Type == "directory" {
+			err := os.MkdirAll(filePath, 0755)
+			if err != nil {
+				t.Error("Unexpected error:", err)
+			}
+
+			c.DestinationPath = v.DestinationPath
+		}
+
+		created = append(created, filePath)
+		stats, _ = os.Stat(filePath)
+
+		go func() {
+			br := bufio.NewReader(r)
+
+			msgCounter := 0
+			for {
+				msg, err := br.ReadString('\n')
+				if err != nil {
+					t.Error("Unexpected error:", err)
+					break
+				}
+
+				if msg != v.ExpectedMessages[msgCounter] {
+					expectedError(t, msg, v.ExpectedMessages[msgCounter])
+				}
+
+				msgCounter++
+			}
+		}()
+
+		err := c.handleItem(filePath, stats, nil)
+		if err != nil {
+			t.Error("Unexpected error:", err)
+		}
+
+		if v.Type == "file" {
+			// Output one more newline for convenience in reading from the pipe
+			fmt.Fprintf(c.scpStdinPipe, "\n")
+		} else if v.Type == "directory" {
+			if !reflect.DeepEqual(c.DestinationPath, v.ExpectedDestinationPath) {
+				expectedError(t, c.DestinationPath, v.ExpectedDestinationPath)
+			}
+		}
+
+		os.Remove(filePath)
+	}
+}
+
 /*
 todo
-handleItem
 cancel
-
-
 */
